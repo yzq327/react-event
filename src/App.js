0fc675708830fd145a6bbb51cef862b35cdea3f8
Yang Zhengqing <1370483456@qq.com>
2022-01-07 15:17:26 +0800

    添加React捕/冒泡和原生捕获/冒泡的比较

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  parentRef;
+  childRef;
+  constructor(props) {
+    super(props);
+    this.parentRef = React.createRef();
+    this.childRef = React.createRef();
+  }
+  componentDidMount() {
+    document.addEventListener(
+      "click",
+      (e) => {
+        console.log("原生事件：document DOM 事件捕获---");
+      },
+      true
+    );
+    this.parentRef.current?.addEventListener(
+      "click",
+      () => {
+        console.log("原生事件：父元素 DOM 事件捕获---");
+      },
+      true
+    );
+    this.childRef.current?.addEventListener(
+      "click",
+      () => {
+        console.log("原生事件：子元素 DOM 事件捕获---");
+      },
+      true
+    );
+    document.addEventListener(
+      "click",
+      (e) => {
+        console.log("原生事件：document DOM 事件冒泡！");
+      },
+      false
+    );
+    this.parentRef.current?.addEventListener(
+      "click",
+      () => {
+        console.log("原生事件：父元素 DOM 事件冒泡！");
+      },
+      false
+    );
+    this.childRef.current?.addEventListener(
+      "click",
+      () => {
+        console.log("原生事件：子元素 DOM 事件冒泡！");
+      },
+      false
+    );
+  }
+  parentClickFun = () => {
+    console.log("React 事件：父元素事件冒泡！");
+  };
+  parentCapture = () => {
+    console.log("React 事件：父元素事件捕获-----");
+  };
+  childClickFun = () => {
+    console.log("React 事件：子元素事件冒泡！");
+  };
+  childCapture = () => {
+    console.log("React 事件：子元素事件捕获-----");
+  };
+  render() {
+    return (
+      <div
+        ref={this.parentRef}
+        onClick={this.parentClickFun}
+        onClickCapture={this.parentCapture}
+      >
+        <button
+          ref={this.childRef}
+          onClick={this.childClickFun}
+          onClickCapture={this.childCapture}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          分析事件执行顺序
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
